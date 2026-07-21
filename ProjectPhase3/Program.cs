b81008781d05aee0791e0c78e0e6a74a27ccf27f
Badr Almadhi <amadhi1995@gmail.com>
2026-07-20 23:31:33 -0600

    added ASP.NET Core Identity autherntication

--- ProjectPhase3/Program.cs
+++ ProjectPhase3/Program.cs
@@ -1,40 +1,67 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ProjectPhase3.Areas.Identity.Data;
 using ProjectPhase3.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Connection to databases
-var connectionString =
+// LMS1: courses, students, assignments, enrollments, etc.
+var lmsConnectionString =
     builder.Configuration["LMS:ConnectionString"]
-    ?? throw new InvalidOperationException("Connection string not found.");
+    ?? throw new InvalidOperationException(
+        "LMS connection string was not found.");
 
+// LMSUsers1: accounts, passwords, roles and login information.
+var identityConnectionString =
+    builder.Configuration["LMS:IdentityConnectionString"]
+    ?? throw new InvalidOperationException(
+        "Identity connection string was not found.");
+
+// Connect LmsContext to LMS1.
 builder.Services.AddDbContext<LmsContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(lmsConnectionString));
 
-// Add services to the container.
+// Connect IdentityContext to LMSUsers1.
+builder.Services.AddDbContext<IdentityContext>(options =>
+    options.UseNpgsql(identityConnectionString));
+
+// Add ASP.NET Core Identity.
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = true;
+    })
+    .AddEntityFrameworkStores<IdentityContext>();
+
+// Add MVC and Razor Pages.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
+// Identify who the user is first.
+app.UseAuthentication();
+
+// Then check what the user is allowed to access.
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+// Makes /Identity/Account/Register and other Identity pages reachable.
+app.MapRazorPages();
 
 app.Run();
